8bd4438bcd55657a376243f15bfb17c55eeb8244
BorjaManterola <bjmanterola@miuandes.cl>
2024-10-23 20:56:13 -0300

    casi con matches

--- frontend/src/components/jsx/Matches.jsx
+++ frontend/src/components/jsx/Matches.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Container, Box, TextField, IconButton, List, ListItem, ListItemText, Divider, Typography, Paper, Button, useTheme, useMediaQuery } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import { useNavigate } from 'react-router-dom';
-import '../css/Matches.css';
 
 function Matches() {
   const [searchTerm, setSearchTerm] = useState('');
   const [matches, setMatches] = useState([]);
+  const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const current_user = JSON.parse(localStorage.getItem('current_user'));
@@ -42,6 +42,17 @@ function Matches() {
   };
 
   useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(`http://localhost:8000/api/products`);
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
     handleSearchMatch();
   }, []);
 
@@ -58,11 +69,17 @@ function Matches() {
 
   return (
     current_user ? (
-      <Container sx={{ height: '100vh', overflowY: 'auto', marginTop: isSmallScreen ? '30%' : '0', marginTop: isMediumScreen ? "30%" : "0" }}>
-        <Typography variant="h4" sx={{ fontFamily: "Belwe" }}>
+      <Container sx={{ height: '100vh', overflowY: 'auto', marginTop: isSmallScreen ? '30%' : '0', marginTop: isMediumScreen ? "30%" : "0", fontFamily: 'Montserrat, sans-serif' }}>
+        <Typography variant="h4" sx={{ fontFamily: "Montserrat, sans-serif", color: "#333" }}>
           Matches
         </Typography>
-        <Box className="boxTodo">
+        <Box sx={{
+          backgroundColor: '#f9f9f9',
+          padding: '20px',
+          borderRadius: '8px',
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+          fontFamily: 'Montserrat, sans-serif',
+        }}>
           <Box 
             sx={{
               position: 'sticky',
@@ -70,27 +87,51 @@ function Matches() {
               zIndex: 1,
               display: 'flex',
               alignItems: 'center',
-              backgroundColor: '#d7b49e',
+              backgroundColor: '#add8e6', // Color lightblue
               padding: '10px',
               borderRadius: '8px',
               marginBottom: '10px',
               marginTop: '15px',
-              border: '1px solid black'  // Add border style
+              border: '1px solid #ccc',  // Add border style
             }}
           >
             <TextField
               label="Search"
               variant="outlined"
-              className="searchInput"
               value={searchTerm}
               onChange={handleChange}
+              sx={{
+                width: '100%', // Ancho del 100%
+                backgroundColor: '#fff', // Fondo blanco
+                '& .MuiOutlinedInput-root': {
+                  backgroundColor: '#fff', // Fondo blanco
+                  '& fieldset': {
+                    borderColor: '#ccc',
+                  },
+                  '&:hover fieldset': {
+                    borderColor: '#888',
+                  },
+                  '&.Mui-focused fieldset': {
+                    borderColor: '#555',
+                  },
+                },
+                '& .MuiInputLabel-root': {
+                  color: '#555',
+                },
+                '& .MuiInputLabel-root.Mui-focused': {
+                  color: '#333',
+                },
+                '& .MuiOutlinedInput-input': {
+                  color: '#333',
+                },
+              }}
             />
-            <IconButton onClick={handleSearchMatch} sx={{ marginLeft: 1 }}>
+            <IconButton onClick={handleSearchMatch} sx={{ marginLeft: 1, color: '#333' }}>
               <SearchIcon />
             </IconButton>
           </Box>
 
-          <Paper elevation={3} sx={{ backgroundColor: '#F8F4E1', fontFamily: "Belwe", overflowY: 'auto', maxHeight: '70vh' }}>
+          <Paper elevation={3} sx={{ backgroundColor: '#fff', fontFamily: "Montserrat, sans-serif", overflowY: 'auto', maxHeight: '70vh', padding: '10px' }}>
             <List>
               {loading && <Typography>Loading...</Typography>}
               {!loading && matches.length === 0 && <Typography>No matches found.</Typography>}
@@ -99,25 +140,8 @@ function Matches() {
                   <ListItem>
                     <ListItemText
                       primary={match.name}
-                      secondary={
-                        match.address ? (
-                          <>
-                            <Typography component="span" variant="body2" color="text.primary">
-                              {match.address.line1 || 'N/A'} , {match.address.line2 || 'N/A'}
-                            </Typography>
-                            <br />
-                            <Typography component="span" variant="body2" color="text.secondary">
-                              {match.address.country ? match.address.country.name : 'N/A'} , {match.address.city || 'N/A'}
-                            </Typography>
-                          </>
-                        ) : (
-                          <Typography component="span" variant="body2" color="text.secondary">
-                            Address not available
-                          </Typography>
-                        )
-                      }
                     />
-                    <Button variant="contained" class="buttonView" onClick={() => handleViewClick(match.id)}>
+                    <Button variant="contained" sx={{ backgroundColor: '#333', '&:hover': { backgroundColor: '#555' }, fontFamily: "Montserrat, sans-serif" }} onClick={() => handleViewClick(match.id)}>
                       View
                     </Button>
                   </ListItem>
@@ -129,9 +153,15 @@ function Matches() {
         </Box>
       </Container>
     ) : (
-      <Container sx={{ height: '100vh', overflowY: 'auto', marginTop: isSmallScreen ? '30%' : '0', marginTop: isMediumScreen ? "30%" : "0" }}>
-        <Box className="boxTodo">
-          <Typography variant="h3" sx={{ fontFamily: "Belwe", color: "#000000", padding: "20px", marginTop: "50px"}}>
+      <Container sx={{ height: '100vh', overflowY: 'auto', marginTop: isSmallScreen ? '30%' : '0', marginTop: isMediumScreen ? "30%" : "0", fontFamily: 'Montserrat, sans-serif' }}>
+        <Box sx={{
+          backgroundColor: '#f9f9f9',
+          padding: '20px',
+          borderRadius: '8px',
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+          fontFamily: 'Montserrat, sans-serif',
+        }}>
+          <Typography variant="h3" sx={{ fontFamily: "Montserrat, sans-serif", color: "#000", padding: "20px", marginTop: "50px"}}>
             Error 401
           </Typography>
         </Box>
