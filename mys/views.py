f4da2fae9f72018ab6ee4b33dcaa4c263d4e8b8f
BorjaManterola <bjmanterola@miuandes.cl>
2024-10-24 12:28:12 -0300

    matches listoko

--- mys/views.py
+++ mys/views.py
@@ -5,7 +5,7 @@ from rest_framework import generics, status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
@@ -22,18 +22,18 @@ class CustomAuthToken(ObtainAuthToken):
 class CurrentUser(generics.RetrieveAPIView):
     def get_object(self):
         return self.request.user
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     serializer_class = UserSerializer
 
 class UserListCreate(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 class UserRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 class ShopListCreate(generics.ListCreateAPIView):
     queryset = Shop.objects.all()
@@ -42,32 +42,32 @@ class ShopListCreate(generics.ListCreateAPIView):
 class ShopRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 class ProductListCreate(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 class ProductRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 class MatchUserProductListCreate(generics.ListCreateAPIView):
     queryset = MatchUserProduct.objects.all()
     serializer_class = MatchUserProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 class MatchUserProductRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = MatchUserProduct.objects.all()
     serializer_class = MatchUserProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 class MatchUser(generics.ListCreateAPIView):
     queryset = MatchUserProduct.objects.all()
     serializer_class = MatchUserProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     def get_queryset(self):
         return MatchUserProduct.objects.filter(user=self.request.user)
 
