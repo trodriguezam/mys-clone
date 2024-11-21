41dad76c793c3154257ebf6d2aa86f58ef6d7768
trodriguezam <trodriguez4@miuandes.cl>
2024-11-21 20:55:49 -0300

    fix: updated backend url

--- frontend/src/components/PageElements/axiosInstance.jsx
+++ frontend/src/components/PageElements/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://mys-backend-de75ad11f796.herokuapp.com/api',
 });
 
 export default axiosInstance;
