6888bc714db3bfed1414f6c1784615c6c2031bf7
trodriguezam <trodriguez4@miuandes.cl>
2024-11-21 21:03:29 -0300

    fix: added production url to CORS permitted urls

--- backend/settings.py
+++ backend/settings.py
@@ -57,7 +57,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://mys-clone.vercel.app'
 ]
 
 ROOT_URLCONF = 'backend.urls'
